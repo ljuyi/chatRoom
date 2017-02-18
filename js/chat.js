1fd4eb6fda72dcc3d83c27f78666c22535b7c070
ljuyi <981765731@qq.com>
2017-02-18 19:25:58 +0800

    update scroll function

--- js/chat.js
+++ js/chat.js
@@ -17,28 +17,46 @@ function fontSelf() {
     var socket = io.connect('http://localhost:4000?roomNum=' + roomNum + '&name=' + name);   //建立连接请求，并发送房间号到服务器端
 
 
+    $('.ui-list').css({'height':document.documentElement.clientHeight - $('.ui-header').height() - $('footer').height()});
+    function postMes(){
+        socket.emit('chatMessage', {'room':roomNum,'name':name ,'mes': $('#message').val()});
+        $('#message').val('');
+    }
+
+    function emitMes(data, cName){
+        if(data.room == roomNum){
+            $('.ui-list').append('<li><h4 class="' + cName + '">'+data.mes+'</h4><p>'+data.name+'</p></li>');
+        }
+    }
+    function updataScroll(){
+        $('.ui-list').scrollTop($('.ui-list')[0].scrollHeight);
+    }
+
+
     $('#roomNum').html('('+roomNum+')');
 
     /*按下回车键发送消息*/
+    $('#postMessage').on('click', function(){
+       postMes();
+    })
+
     $('#message').on('keypress', function(event){
         if(event.keyCode == 13){
-            socket.emit('chatMessage', {'room':roomNum,'name':name ,'mes': $('#message').val()});
-            $('#message').val('');
+            postMes();
         }
     })
+
     socket.on('chatMessage', function(data){
-        if(data.room == roomNum){
-            $('.ui-tab-content .ui-list').append('<li><p>'+data.name+'</p><h4 class="dialog">'+data.mes+'</h4></li>');
-        }
+        emitMes(data, 'dialog');
+        updataScroll();
+
     })
     socket.on('newFriend', function(data){
-        if(data.room == roomNum){
-            $('.ui-tab-content .ui-list').append('<li><p>'+data.name+'</p><h4 class="dialog">'+data.mes+'</h4></li>');
-        }
+        emitMes(data, 'dialog');
+        updataScroll();
     })
     socket.on('myMessage', function(data){
-        if(data.room == roomNum){
-            $('.ui-tab-content .ui-list').append('<li><h4 class="my-dialog">'+data.mes+'</h4><p>'+data.name+'</p></li>');
-        }
+        emitMes(data, 'my-dialog');
+        updataScroll();
     })
 })();
